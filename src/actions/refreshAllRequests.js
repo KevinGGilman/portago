9737e048acca2e88d9d10667c7b7deeafaf3b9c4
Kevin Gervais Gilman <kevingervaisgilman@MacBook-Pro-de-Kevin.local>
2019-02-13 03:43:20 -0500

    speed up refresh

--- src/actions/refreshAllRequests.js
+++ src/actions/refreshAllRequests.js
@@ -1,34 +1,22 @@
 export default function refreshAllRequests (isRefreshUser, globalCallback) {
   const global = this.props.global || this.state
-  let stateAccumulator = { isLoading: false }
-  let total = 0
-  let count = 0
 
   const get = (url, req, callback) => {
-    total++
     global.socket.emit(url, req, (err, result) => {
-      count++
       if (err) return console.log(err)
       callback(result)
-      if (count === total) {
-        global.setState(stateAccumulator, () => {
-          if (globalCallback) globalCallback(stateAccumulator)
-        })
-      }
     })
   }
-  const preState = (state) => {
-    stateAccumulator = { ...stateAccumulator, ...state }
-  }
+  const setState = (state) => global.setState(state)
 
-  if (isRefreshUser) get('users/self', {}, (obj) => preState({ user: obj }))
-  get('carousel/list', {}, (arr) => preState({ carousel: arr }))
-  get('locations/list', {}, (arr) => preState({ locationList: arr }))
-  get('posts/list', {}, (arr) => preState({ postList: arr }))
-  get('articles/list', { type: 'pocket' }, (arr) => preState({ pocketList: arr }))
-  get('articles/list', { type: 'straw' }, (arr) => preState({ strawList: arr }))
-  get('articles/list', { type: 'brush' }, (arr) => preState({ brushList: arr }))
-  get('categories/list', {}, (arr) => preState({
+  if (isRefreshUser) get('users/self', {}, (obj) => setState({ user: obj }))
+  get('carousel/list', {}, (arr) => setState({ carousel: arr }))
+  get('locations/list', {}, (arr) => setState({ locationList: arr }))
+  get('posts/list', {}, (arr) => setState({ postList: arr }))
+  get('articles/list', { type: 'pocket' }, (arr) => setState({ pocketList: arr }))
+  get('articles/list', { type: 'straw' }, (arr) => setState({ strawList: arr }))
+  get('articles/list', { type: 'brush' }, (arr) => setState({ brushList: arr }))
+  get('categories/list', {}, (arr) => setState({
     categoryList: arr,
     categoryMap: global.do.listToMap(arr)
   }))
