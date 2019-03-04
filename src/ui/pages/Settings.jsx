907018aad624ab1938a656dbd4f01750ff72fa18
Kevin Gervais Gilman <kevingervaisgilman@MacBook-Pro-de-Kevin.local>
2019-03-03 20:40:15 -0500

    mobile fix

--- src/ui/pages/Settings.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { SideNav } from '../SideNav'
-export const Settings = (props) => (
-  <div className='settings'>
-    <SideNav global={props.global} />
-  </div>
-)
